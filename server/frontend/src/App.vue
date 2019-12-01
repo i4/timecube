ca54495e03d9b2785e1976fd375a8006cc69399a
Peter Kranz <peter@myref.net>
2019-12-01 22:21:11 +0100

    move timecube frontend code to new vue project

--- server/frontend/src/App.vue
+++ server/frontend/src/App.vue
@@ -1,29 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+	<div id="app">
+		<dashboard />
+	</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Dashboard from "./components/Dashboard.vue";
 
 @Component({
-  components: {
-    HelloWorld
-  }
+	components: {
+		Dashboard
+	}
 })
 export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ server/frontend/src/components/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Dashboard extends Vue {
+	@Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
